7fd0de3bd45e82a66ac192bf94cd3000499d5a87
Tanish407 <113087488+Tanish407@users.noreply.github.com>
2022-09-14 05:59:42 -0700

    Add files via upload

---
+++ string.py
@@ -0,0 +1,37 @@
+from os import replace
+
+
+s="RJGP is a college"
+# print(s.upper())
+
+# print(s.lower())
+
+# print(len(s))
+
+# print(s.replace("RJGP","GPW"))
+
+# print(s.capitalize())
+
+# print(s.isupper())
+
+# print(s.isnumeric())
+
+# print(s.isspace())
+
+# print(s[3])
+
+# print(s[0:5])
+
+# print(s.startswith("GPW"))
+
+# print(s.find("RJGP"))
+
+# print(s.title())
+
+# print(s.casefold())
+
+print(s.split('RJGP'))
+
+
+
+
