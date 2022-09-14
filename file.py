c9dba13d85a74d1be595963f2f7674bec96de457
Tanish407 <113087488+Tanish407@users.noreply.github.com>
2022-09-13 23:19:59 -0700

    Update file.py

--- file.py
+++ file.py
@@ -1,20 +1,20 @@
-# f=open('new.txt','w')
-# f.write("hello")
-# f.close()
+f=open('new.txt','w')
+f.write("hello")
+f.close()
 
-# f=open('ne.txt','a')
-# f.write("CSE")
-# f.close()
+f=open('ne.txt','a')
+f.write("CSE")
+f.close()
 
-# import os
-# os.remove('new.txt')
+import os
+os.remove('new.txt')
 
-# with open('ne.txt','w') as f:
-#     f.write("hello")
+with open('ne.txt','w') as f:
+  f.write("hello")
 
-# f=open('ne.txt','r')
-# print(f.readline())
-# f.close()
+f=open('ne.txt','r')
+print(f.readline())
+f.close()
 
 f=open('ne.txt','r')
 print (f.read())
