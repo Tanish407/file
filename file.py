3e41f0b2637a22ae258a7a5fcf7dc761cdbf63e5
Tanish407 <tanishsingla934@gmail.com>
2022-09-14 11:06:50 -0700

    first commit

---
+++ file.py
@@ -0,0 +1,21 @@
+# f=open('new.txt','w')
+# f.write("hello")
+# f.close()
+
+# f=open('ne.txt','a')
+# f.write("CSE")
+# f.close()
+
+# import os
+# os.remove('new.txt')
+
+# with open('ne.txt','w') as f:
+#     f.write("hello")
+
+# f=open('ne.txt','r')
+# print(f.readline())
+# f.close()
+
+f=open('ne.txt','r')
+print (f.read())
+f.close()
